a33ac4cdfd68aac593e6168a582f463676adcb5a
Top_Neox <top@neox.ru>
2013-06-24 11:37:16 +0400

    ADD: отображение категорий в тулбаре

--- Articles/Structure/Article/Admin/Model.php
+++ Articles/Structure/Article/Admin/Model.php
@@ -1,7 +1,33 @@
 <?php
 namespace Articles\Structure\Article\Admin;
 
+use Ideal\Core\Db;
+use Ideal\Core\Request;
+
 class Model extends \Ideal\Structure\Roster\Admin\ModelAbstract
 {
+    public function getToolbar()
+    {
+        $db = Db::getInstance();
+        $_table = 'i_articles_structure_category';
+        $structurePath = '11-1';
+        $_sql = "SELECT * FROM {$_table} WHERE structure_path='{$structurePath}' AND is_active=1 ORDER BY cid";
+        $this->categories = $db->queryArray($_sql);
+
+        $request = new Request();
+        $currentCategory = $request->category;
+
+        $select = '<select name="category">';
+        foreach ($this->categories as $category) {
+            $selected = '';
+            if ($category['ID'] == $currentCategory) {
+                $selected = 'selected="selected"';
+            }
+            $select .= '<option ' . $selected . ' value="' . $category['ID'] . '">' . $category['name'] . '</option>';
+        }
+        $select .= '</select>';
+
+        return $select;
+    }
 
 }
