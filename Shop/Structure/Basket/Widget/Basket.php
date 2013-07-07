63ec6179eedd7cf0592e4b8db7a22d71b7aef2e6
Top_Neox <top@neox.ru>
2013-07-07 11:36:48 +0400

    Shop: исправил виджет Basket и обращение к его данным в шаблоне

--- Shop/Structure/Basket/Widget/Basket.php
+++ Shop/Structure/Basket/Widget/Basket.php
@@ -14,29 +14,14 @@ class Basket extends \Ideal\Core\Widget
 
     public function getData()
     {
-        $db = Db::getInstance();
-        $config = Config::getInstance();
-
-        // Считываем список новостей
-        $table = $config->db['prefix'] . 'ideal_structure_news';
-        $_sql = 'SELECT ID, name, annot, date_create, img, url
-                 FROM ' . $table . '
-                 WHERE is_active=1
-                 ORDER BY date_create DESC
-                 LIMIT ' . intval($num);
-        $news = $db->queryArray($_sql);
-        $freshNews = array();
-        $num = 0;
-        foreach ($news as $v) {
-            $freshNews[$num]['name'] = $v['name'];
-            $freshNews[$num]['url'] = $v['url'];
-            $freshNews[$num]['img'] = $v['img'];
-            $freshNews[$num]['annot'] = $v['annot'];
-            $freshNews[$num]['date'] = Util::dateReach($v['date_create']);
-            $num++;
-        }
-
-        return $freshNews;        
+        // TODO экранировать получене данных от кук, это уязвимость
+        $basket = array(
+            'amount' => 0,
+            'price' => 0
+        );
+        if (isset($_COOKIE['basket']))
+            $basket = json_decode($_COOKIE['basket'], true);
+        return $basket;
     }
 
 }
