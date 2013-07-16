4836dc2b08ed34cc5c118e3874ce30a24bed2d75
Top_Neox <top@neox.ru>
2013-07-16 10:45:26 +0400

    Articles: сделал фильтр статей по категориям

--- Articles/Structure/Article/Admin/Model.php
+++ Articles/Structure/Article/Admin/Model.php
@@ -17,7 +17,7 @@ class Model extends \Ideal\Structure\Roster\Admin\ModelAbstract
         $request = new Request();
         $currentCategory = $request->category;
 
-        $select = '<select name="category">';
+        $select = '<select name="category"><option value="">Все статьи</option>';
         foreach ($this->categories as $category) {
             $selected = '';
             if ($category['ID'] == $currentCategory) {
@@ -30,4 +30,65 @@ class Model extends \Ideal\Structure\Roster\Admin\ModelAbstract
         return $select;
     }
 
+
+    /**
+     * @param int $page Номер отображаемой страницы
+     * @param int $onPage Кол-во элементов на странице
+     * @return array Полученный список элементов
+     */
+    public function getList($page, $onPage)
+    {
+        $where = $this->getWhere("structure_path='{$this->structurePath}'");
+
+        if ($page == 0) $page = 1;
+        $start = ($page - 1) * $onPage;
+
+        $_sql = "SELECT * FROM {$this->_table} WHERE {$where}
+                          ORDER BY {$this->params['field_sort']} LIMIT {$start}, {$onPage}";
+        $db = Db::getInstance();
+        $list = $db->queryArray($_sql);
+
+        return $list;
+    }
+
+
+    /**
+     * Получить общее количество элементов в списке
+     * @return array Полученный список элементов
+     */
+    public function getListCount()
+    {
+        $db = Db::getInstance();
+
+        $where = $this->getWhere("structure_path='{$this->structurePath}'");
+
+        // Считываем все элементы первого уровня
+        $_sql = "SELECT COUNT(ID) FROM {$this->_table} WHERE {$where}";
+        $list = $db->queryArray($_sql);
+
+        return $list[0]['COUNT(ID)'];
+    }
+
+
+    /**
+     * Добавление к where-запросу фильтра по category_id
+     * @param $where Исходная WHERE-часть
+     * @return string Модифицированная WHERE-часть, с расширенным запросом, если установлена GET-переменная category
+     */
+    protected function getWhere($where)
+    {
+        $request = new Request();
+        $currentCategory = $request->category;
+
+        if ($currentCategory != '') {
+            if ($where != '') {
+                $where .= ' AND ';
+            }
+            // Выборка статей, принадлежащих этой категории
+            $where .= 'ID IN (SELECT article_id FROM i_articles_category_article WHERE category_id='
+                . mysql_real_escape_string($currentCategory) . ')';
+        }
+
+        return $where;
+    }
 }
