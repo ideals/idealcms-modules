7ca36aa54b284a3dd72b923ddf51b32ecac0afa5
Help2_Neox <help2@neox.ru>
2013-08-12 18:24:02 +0400

    Shop: Вернул возврат товаров

--- Shop/Structure/CategoryMulti/Site/Model.php
+++ Shop/Structure/CategoryMulti/Site/Model.php
@@ -5,25 +5,17 @@ use Ideal\Core\Db;
 
 class Model extends \Ideal\Structure\Part\Site\ModelAbstract
 {
-    public function getWhere($where)
+    public function getGoods()
     {
+        $page = ($_GET['page']) ? $_GET['page'] : 1;
+        $from = $this->limit * ($page - 1);;
+
         $db = Db::getInstance();
         $categoryId = $this->object['ID'];
-        $_sql = "SELECT good_id FROM i_shop_category_good WHERE category_id='{$categoryId}'";
-        $goodIdsArr = $db->queryArray($_sql);
-		if (count($goodIdsArr) == 0) {
-			return array();
-		}
-        $goodIs = array();
-        foreach ($goodIdsArr as $good) {
-            $goodIs[] = "'" . $good['good_id'] . "'";
-        }
-        $goodIs = implode(',', $goodIs);
-
-        $where .= "AND is_active=1 AND ID IN ({$goodIs})";
-
-        $this->params['field_sort'] = 'name';
-
-        return $where;
+       // $_sql = "SELECT * FROM i_shop_structure_categorymulti AS ssc WHERE ssc.ID IN (SELECT  scg.good_id FROM i_shop_category_good AS scg WHERE scg.category_id ='{$categoryId}')";
+        $_sql = "SELECT * FROM i_shop_structure_categorymulti AS ssc
+                    LEFT JOIN i_shop_category_good AS scg ON ssc.ID = scg.good_id WHERE scg.category_id = '{$categoryId}' LIMIT {$from}, {$this->limit}";
+        $goods = $db->queryArray($_sql);
+        return $goods;
     }
 }
