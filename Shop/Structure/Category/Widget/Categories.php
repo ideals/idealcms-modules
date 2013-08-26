77b9f214b76eac58a2120da14ffb7895d4225712
Paul Nikitin <paulnikitin@gmail.com>
2013-08-26 17:41:14 +0400

    Shop: привёл в соответствие с правилами виджет получения одноуровневого списка категорий товара

--- Shop/Structure/Category/Widget/Categories.php
+++ Shop/Structure/Category/Widget/Categories.php
@@ -5,14 +5,37 @@ use Ideal\Core\Db;
 
 class Categories extends \Ideal\Core\Widget
 {
+    public function setStructurePath($structurePath)
+    {
+        $this->structurePath = $structurePath;
+    }
+
+    public function setPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+    }
 
     public function getData()
     {
         $db = Db::getInstance();
-        $_sql = "SELECT name, url, img FROM i_shop_structure_category";
-        $goodGroup = $db->queryArray($_sql);
+        $_sql = "SELECT * FROM i_shop_structure_category
+                    WHERE lvl=1 AND is_active=1 AND is_not_menu=0 AND structure_path='{$this->structurePath}'
+                    ORDER BY cid";
+        $menuList = $db->queryArray($_sql);
 
-        return $goodGroup;
+        $menu = array();
+        $url = new \Ideal\Field\Url\Model();
+        foreach ($menuList as $v) {
+            $v['link'] = $url->getUrlWithPrefix($v, $this->prefix);
+            $menu[$v['cid']] = $v;
+        }
+        unset($menuList);
+
+        $object = $this->model->object;
+        if ($object['structure_path'] == $this->structurePath) {
+            $menu[$object['cid']]['isActivePage'] = 1;
+        }
+        return $menu;
     }
 
 }
