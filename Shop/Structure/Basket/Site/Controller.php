c5d14a3fc09ed3109f5b9df561856a1eb610c5aa
Help1_Neox <help1@neox.ru>
2013-10-04 18:56:50 +0400

    Shop: fix корзины

--- Shop/Structure/Basket/Site/Controller.php
+++ Shop/Structure/Basket/Site/Controller.php
@@ -1,22 +1,11 @@
 <?php
 namespace Shop\Structure\Basket\Site;
 
-class Controller extends \Ideal\Structure\Part\Site\ControllerAbstract
+class Controller extends \Ideal\Core\Site\Controller
 {
     public function indexAction()
     {
-        $this->templateInit();
-
-        $templatesVars = $this->model->getTemplatesVars();
-
-        if (isset($templatesVars['template']['content'])) {
-            list($header, $text) = $this->model->extractHeader($templatesVars['template']['content']);
-            $templatesVars['template']['content'] = $text;
-        }
-
-        foreach($templatesVars as $k => $v) {
-            $this->view->$k = $v;
-        }
+        parent::indexAction();
 
         $this->view->goods = $this->model->getGoods();
     }
