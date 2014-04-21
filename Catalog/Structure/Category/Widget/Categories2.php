a74a96b1590ea95518249d0368c12013cecd3703
Paul Nikitin <paulnikitin@gmail.com>
2014-04-21 15:10:55 +0400

    Переделал виджет Categories2 под prev_structure

--- Catalog/Structure/Category/Widget/Categories2.php
+++ Catalog/Structure/Category/Widget/Categories2.php
@@ -4,36 +4,29 @@ namespace Catalog\Structure\Category\Widget;
 use Ideal\Core\Db;
 use Ideal\Core\Config;
 
+/**
+ * Виджет получения двухуровневого списка категорий товара из БД
+ *
+ */
 class Categories2 extends \Ideal\Core\Widget
 {
-    protected $structurePath;
-    protected $prefix;
-
-    public function setStructurePath($structurePath)
-    {
-        $this->structurePath = $structurePath;
-    }
-
-
-    public function setPrefix($prefix)
-    {
-        $this->prefix = $prefix;
-    }
-
-
+    /**
+     * Получение двухуровневого меню категорий в структуризированном виде
+     *
+     * Ссылки на пункты меню прописываются в свойстве link (без href).
+     * У первого уровня в свойстве subMenu находится массив, содержащий элементы
+     * второго уровня.
+     *
+     * @return array Список категорий из БД
+     */
     public function getData()
     {
         // Определяем кол-во разрядов на один уровень cid для структуры категорий
         $config = Config::getInstance();
-        $category = $config->getStructureByName('Shop_Category');
+        $category = $config->getStructureByName('Catalog_Category');
         $digits = $category['params']['digits'];
 
-        $db = Db::getInstance();
-        $config = Config::getInstance();
-        $_table = $config->db['prefix'] . 'catalog_structure_category';
-        $_sql = "SELECT * FROM {$_table}
-                    WHERE (lvl = 1 OR lvl = 2) AND is_active=1 AND is_not_menu=0 AND structure_path='{$this->structurePath}' ORDER BY cid";
-        $menuList = $db->queryArray($_sql);
+        $menuList = $this->getList();
 
         // Раскладываем считанное меню во вложенные массивы по cid и lvl
         $num = 0;
@@ -42,31 +35,26 @@ class Categories2 extends \Ideal\Core\Widget
         foreach ($menuList as $v) {
             if ($v['lvl'] == 1) {
                 $num = substr($v['cid'], 0, $digits);
-                $parentUrl = $v['url'];
-                if (isset($v['url_full']) && strlen($v['url_full']) > 1) {
-                    $v['link'] = 'href="' . $v['url_full'] . $config->urlSuffix . '"';
-                } else {
-                    $v['link'] = 'href="' . $url->getUrlWithPrefix($v, $this->prefix) . $config->urlSuffix . '"';
-                }
+                $parentUrl = (isset($v['is_skip']) && $v['is_skip']) ? '' : $v['url'];
+                $v['link'] = $url->getUrlWithPrefix($v, $this->prefix);
                 $v['subMenu'] = array();
                 $menu[$num] = $v;
             }
             if ($v['lvl'] == 2) {
                 $prefix = $this->prefix . '/' . $parentUrl;
-                if (isset($v['url_full']) && strlen($v['url_full']) > 1) {
-                    $v['link'] = 'href="' . $v['url_full'] . $config->urlSuffix . '"';
-                } else {
-                    $v['link'] = 'href="' . $url->getUrlWithPrefix($v, $prefix) . $config->urlSuffix . '"';
-                }
+                $v['link'] = $url->getUrlWithPrefix($v, $prefix);
                 $menu[$num]['subMenu'][] = $v;
             }
         }
         unset($menuList);
 
+        // Определение активных пунктов меню
         $pageData = $this->model->getPageData();
-        if (isset($pageData['structure_path']) && $pageData['structure_path'] == $this->structurePath) {
+        if (isset($pageData['prev_structure']) && $pageData['prev_structure'] == $this->prevStructure) {
             $activeUrl = substr($pageData['cid'], 0, $digits);
-            if (!isset($menu[$activeUrl])) return $menu;
+            if (!isset($menu[$activeUrl])) {
+                return $menu;
+            }
             $menu[$activeUrl]['activeUrl'] = 1;
             $menu[$activeUrl]['classActiveUrl'] = 'activeMenu';
             foreach ($menu[$activeUrl]['subMenu'] as $k => $elem) {
@@ -81,4 +69,23 @@ class Categories2 extends \Ideal\Core\Widget
         return $menu;
     }
 
+    /**
+     * Выполняет запрос к БД для получения списка категорий
+     *
+     * Метод сделан максимально просто, чтобы было легче модифицировать получение
+     * категорий в наследниках виджета.
+     *
+     * @return array Список категорий из БД
+     */
+    protected function getList()
+    {
+        $db = Db::getInstance();
+        $config = Config::getInstance();
+        $_table = $config->db['prefix'] . 'catalog_structure_category';
+        $_sql = "SELECT * FROM {$_table}
+                    WHERE (lvl = 1 OR lvl = 2) AND is_active=1 AND is_not_menu=0
+                          AND prev_structure='{$this->prevStructure}' ORDER BY cid";
+        $menuList = $db->queryArray($_sql);
+        return $menuList;
+    }
 }
