66ea79d5f47bb1a5e867d6e594fce754d15aba9e
Николай Конюхов <help4@neox.com>
2014-11-12 16:31:20 +0300

    FIX: если в goodmodel 404 - пердаем её в model. FIX: psr2

--- CatalogPlus/Structure/Category/Site/ControllerAbstract.php
+++ CatalogPlus/Structure/Category/Site/ControllerAbstract.php
@@ -15,12 +15,17 @@ class ControllerAbstract extends \Ideal\Core\Site\Controller
         parent::indexAction();
 
         $request = new Request();
-        $page = intval($request->page);
-        if ($page == 0) $page = 1;
+        $page = intval(substr($request->page, 0, 10));
+        if ($page == 0) {
+            $page = 1;
+        }
 
         $this->goodModel = new \CatalogPlus\Structure\Good\Site\Model('');
         $this->goodModel->setCategoryModel($this->model);
         $this->view->goods = $this->goodModel->getList($page);
+        if ($this->goodModel->is404) {
+            $this->model->is404 = true;
+        }
         $this->view->pager = $this->goodModel->getPager('page');
 
         // TODO реализация вывода списка групп и подгруппы текущей активной группы
