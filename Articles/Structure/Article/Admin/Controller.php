c23be30a4e412afb94b8add067d48f73c41f800d
morwal <devnull@localhost>
2015-03-31 15:19:55 +0500

    Добавлен метод для удаления информации о статье из таблиц структуры статей.

--- Articles/Structure/Article/Admin/Controller.php
+++ Articles/Structure/Article/Admin/Controller.php
@@ -1,6 +1,24 @@
 <?php
 namespace Articles\Structure\Article\Admin;
 
+use Ideal\Core\Request;
+use Ideal\Core\Db;
+use Ideal\Core\Config;
+
 class Controller extends \Ideal\Structure\Roster\Admin\ControllerAbstract
 {
+    public function deleteAction()
+    {
+        $request = new Request();
+        $config = Config::getInstance();
+        $db = Db::getInstance();
+
+        $result = array();
+        $result['ID'] = intval($request->id);
+
+        $catTable = $config->db['prefix'] . 'articles_medium_taglist';
+        $db->query("DELETE FROM {$catTable} WHERE article_id={$result['ID']}");
+
+        parent::deleteAction();
+    }
 }
