aa2907cba52c10b1732e18aea4bdd755b77409d3
Павел Никитин <top@neox.ru>
2015-04-22 10:45:42 +0300

    FIX: завершение подготовки модели обработки выгрузки из 1С к переопределению

---
+++ Shop/Structure/Service/Load1c/Model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Ideal CMS (http://idealcms.ru/)
+ *
+ * @link      http://github.com/ideals/idealcms репозиторий исходного кода
+ * @copyright Copyright (c) 2012-2015 Ideal CMS (http://idealcms.ru)
+ * @license   http://idealcms.ru/license.html LGPL v3
+ */
+
+namespace Shop\Structure\Service\Load1c;
+
+
+class Model extends ModelAbstract
+{
+
+}
