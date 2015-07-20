75231371defa33ddd5e24e655eb1fa567c450543
morwal <devnull@localhost>
2015-07-20 19:07:30 +0500

    Форма в шаблоне по умолчанию "Подтверждение заказа" использует библиотеку "FormPhp"

---
+++ Shop/Structure/Basket/Site/Tabs/AjaxController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Ideal CMS (http://idealcms.ru/)
+ *
+ * @link      http://github.com/ideals/idealcms репозиторий исходного кода
+ * @copyright Copyright (c) 2012-2015 Ideal CMS (http://idealcms.ru)
+ * @license   http://idealcms.ru/license.html LGPL v3
+ */
+namespace Shop\Structure\Basket\Site\Tabs;
+
+use FormPhp\Forms;
+use Ideal\Core\Request;
+
+class AjaxController extends \Ideal\Core\AjaxController
+{
+    // Обрабатывает запросы для формы из шаблона поумолчанию "Подтверждение заказа"
+    public function confirmationAction()
+    {
+        $request = new Request();
+        $form = new Forms('confirmationForm');
+        $form->setAjaxUrl('/');
+        $form->add('order_comments', 'text');
+        if ($form->isPostRequest()) {
+            if ($form->isValid()) {
+                // Если валидация пройдена успешно, то записываем значение в куки
+                setcookie("order_comments", $form->getValue('order_comments'));
+            }
+        } else {
+            // Обработка запросов для получения функциональных частей формы
+            switch ($request->target) {
+                // Генерируем js
+                case 'js':
+                    $script = <<<JS
+                    var successSendForm = false;
+                    $('.next').parent('a').on('click', function () {
+                        if (successSendForm == false) {
+                            $('#confirmationForm').submit();
+                            return false;
+                        } else {
+                            window.location = $(this).attr('href');
+                        }
+                    });
+                    $('#confirmationForm').on('form.successSend', function () {
+                        successSendForm = true;
+                        $('.next').parent('a').click();
+                    });
+JS;
+                    $form->setJs($script);
+                    $request->mode = 'js';
+                    break;
+                // Генерируем css
+                case 'css':
+                    $request->mode = 'css';
+                    break;
+                // Генерируем стартовую часть формы
+                case 'start':
+                    echo $form->start();
+                    exit();
+                    break;
+            }
+            $form->render();
+        }
+        exit();
+    }
+}
