e393a4f26b080d85dfd863c865123766208f9fd2
Николай Конюхов <help4@neox.com>
2015-07-22 17:41:22 +0300

    ADD: файлы для админки

---
+++ Shop/Structure/Service/Load1c_v2/Action.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Help4
+ * Date: 22.07.2015
+ * Time: 17:35
+ */
+echo 'hello world';
---
+++ Shop/Structure/Service/Load1c_v2/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'name'   => 'Загрузка из 1C. Версия 2',
+    'pos'    => 31,
+    'info'   => '',
+    'structure' => 'Ideal_Service',
+    'ID' => 'Shop_Load1cV2'
+);
