839f656f03d2c69fd8dbdc92cb8be17d9b26f495
morwal <devnull@localhost>
2015-09-08 18:13:48 +0500

    Вывод индивидуальной страницы заказа

--- Shop/Structure/Order/Site/ControllerAbstract.php
+++ Shop/Structure/Order/Site/ControllerAbstract.php
@@ -5,10 +5,4 @@ class ControllerAbstract extends \Ideal\Structure\Part\Site\Controller
 {
     /** @var  Model model */
     protected $model;
-
-    public function indexAction()
-    {
-        parent::indexAction();
-        $this->view->goods = $this->model->getGoods();
-    }
 }
