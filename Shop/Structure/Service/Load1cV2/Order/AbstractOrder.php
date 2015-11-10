c397c234a4e9fb9f3ac84daae56f61a982c940ff
morwal <devnull@localhost>
2015-11-10 15:24:01 +0500

    FIX: В процессе выгрузки заказов в 1С для контрагентов так же передаются поля "Электронная почта" и "Телефон рабочий"

--- Shop/Structure/Service/Load1cV2/Order/AbstractOrder.php
+++ Shop/Structure/Service/Load1cV2/Order/AbstractOrder.php
@@ -27,14 +27,17 @@ class AbstractOrder
 
         $i = $config->db['prefix'];
         $orderSql = "SELECT sho.id, sho.date_create, sho.name, sho.price,".
-            " csu.name as buyerName, csu.last_name as buyerLastName, csu.ID as buyerID,".
+            " csu.name as buyerName, csu.last_name as buyerLastName, csu.ID as buyerID, csu.address as buyerAddress, csu.phone as buyerPhone, csu.email as buyerEmail,".
             " d.good_id_1c, d.offer_id_1c, d.count, d.sum,stg.currency, sto.name as full_name, stg.coefficient, sto.price as fe".
             " FROM {$i}shop_structure_order sho".
             " LEFT JOIN {$i}shop_structure_orderdetail d on d.order_id=sho.id".
             " LEFT JOIN {$i}catalogplus_structure_good stg on d.good_id_1c=stg.id_1c".
             " LEFT JOIN {$i}cabinet_structure_user csu on csu.ID = sho.user_id".
             " LEFT JOIN {$i}catalogplus_structure_offer sto on sto.offer_id=d.offer_id_1c".
-            " where sho.goods_id<>'' AND sho.export=1 AND LIMIT 0,200";
+
+            // В этих условиях задана проверка на обязательное наличие зарегистрированного пользователя
+            // Если заказ сделал анонимный пользователь, то он, пока, не попадёт в 1С.
+            " where sho.goods_id<>'' AND sho.export=1 AND csu.name IS NOT NULL LIMIT 0,200";
 
         $orderList = $db->select($orderSql);
         if (count($orderList) === 0) {
@@ -43,10 +46,10 @@ class AbstractOrder
         $items = array();
         foreach ($orderList as $element) {
             if (isset($items[$element['id']])) {
-                $items[$element['id']]['goods'][] = array_slice($element, 7);
+                $items[$element['id']]['goods'][] = array_slice($element, 10);
             } else {
-                $items[$element['id']] = array_slice($element, 0, 7);
-                $items[$element['id']]['goods'][] = array_slice($element, 7);
+                $items[$element['id']] = array_slice($element, 0, 10);
+                $items[$element['id']]['goods'][] = array_slice($element, 10);
             }
         }
         unset($orderList);
@@ -79,13 +82,38 @@ class AbstractOrder
             $guid = substr($charid, 0, 8) . '-' . substr($charid, 8, 4) . '-' . substr($charid, 12, 4) . '-' .
                 substr($charid, 16, 4) . '-' . substr($charid, 20, 12);*/
 
+            $agent->addChild("Ид", $item['buyerID']);
+            $agent->addChild("Имя", $item['buyerName']);
+
             // Формируем наименование покапателя
             $buyerName = $item['buyerName'];
             if (!empty($item['buyerLastName'])) {
                 $buyerName .= ' ' . $item['buyerLastName'];
+                $agent->addChild("Фамилия", $item['buyerLastName']);
             }
 
-            $agent->addChild("Ид", $item['buyerID']);
+            $regAddress = $agent->addChild("АдресРегистрации");
+            // Если у заказчика указан адрес, то передаём его в 1С
+            if (!empty($item['buyerAddress'])) {
+                $regAddress->addChild('Представление', $item['buyerAddress']);
+            }
+
+            $contacts = $agent->addChild('Контакты');
+
+            // Если у заказчика указан телефон, то передаём его в 1С
+            if (!empty($item['buyerPhone'])) {
+                $buyerPhoneContact = $contacts->addChild('Контакт');
+                $buyerPhoneContact->addChild('Тип', 'Телефон рабочий');
+                $buyerPhoneContact->addChild('Значение', $item['buyerPhone']);
+            }
+
+            // Если у заказчика указан e-mail, то передаём его в 1С
+            if (!empty($item['buyerEmail'])) {
+                $buyerEmailContact = $contacts->addChild('Контакт');
+                $buyerEmailContact->addChild('Тип', 'Электронная почта');
+                $buyerEmailContact->addChild('Значение', $item['buyerEmail']);
+            }
+
             $agent->addChild("Наименование", $buyerName);
             $agent->addChild("Роль", "Покупатель");
             $agent->addChild("ПолноеНаименование", $buyerName);
