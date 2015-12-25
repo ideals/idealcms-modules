b9899b756aaa1ef6fb8a4d291a60846d912683e2
Валентин Ломов <devnull@localhost>
2015-12-25 18:38:05 +0500

    FIX: В структуры модуля "Cabinet" добавлены дополнительные классы для удобного переопределения в проектах

---
+++ Cabinet/Structure/Part/Site/AccountForms/AccountFormsAbstract.php
@@ -0,0 +1,188 @@
+<?php
+namespace Cabinet\Structure\Part\Site\AccountForms;
+
+use FormPhp;
+use Ideal\Core\View;
+use Ideal\Core\Config;
+
+/**
+ * Класс обеспечивающий генерацию объектов и html-представлений форм, связанных с личным кабинетом
+ */
+class AccountFormsAbstract
+{
+
+    /** @var string Адрес страницы, на которой располагается форма */
+    protected $link = '';
+
+    public function setLink($link)
+    {
+        $this->link = $link;
+    }
+
+    /**
+     * Генерирует html формы входа
+     *
+     * @return string
+     */
+    public function getLoginForm()
+    {
+        $form = $this->getloginFormObject();
+        $formView = $this->templateInit('Cabinet/Structure/Part/Site/AccountForms/loginForm.twig');
+        $formView->start = $form->start();
+        $formView->link = $this->link;
+        $formHtml = $formView->render();
+        return $formHtml;
+    }
+
+    /**
+     * Генерирует объект формы входа
+     *
+     * @return \FormPhp\Forms
+     * @throws \Exception
+     */
+    public function getLoginFormObject()
+    {
+        $form = new FormPhp\Forms('loginForm');
+        $form->setAjaxUrl('/?mode=ajax&controller=\\\\Cabinet\\\\Structure\\\\Part\\\\Site&action=login');
+        $form->add('login', 'text');
+        $form->add('pass', 'text');
+        $form->setValidator('login', 'required');
+        $form->setValidator('login', 'email');
+        $form->setValidator('pass', 'required');
+        return $form;
+    }
+
+    /**
+     * Генерирует html формы личного кабинета
+     *
+     * @return string
+     */
+    public function getLkForm($user)
+    {
+        $form = $this->getLkFormObject();
+        $formView = $this->templateInit('Cabinet/Structure/Part/Site/AccountForms/lkForm.twig');
+        $formView->start = $form->start();
+        $formView->user = $user;
+        $formHtml = $formView->render();
+        return $formHtml;
+    }
+
+    /**
+     * Генерирует объект формы личного кабинета
+     *
+     * @return \FormPhp\Forms
+     * @throws \Exception
+     */
+    public function getLkFormObject()
+    {
+        $form = new FormPhp\Forms('lkForm');
+        $form->setAjaxUrl('/?mode=ajax&controller=\\\\Cabinet\\\\Structure\\\\Part\\\\Site&action=save');
+        $form->add('fname', 'text');
+        $form->add('phone', 'text');
+        $form->add('addr', 'text');
+        $form->add('pass', 'text');
+        $form->setValidator('phone', 'phone');
+        return $form;
+    }
+
+    /**
+     * Генерирует html формы восстановления пароля
+     *
+     * @return string
+     */
+    public function getRestoreForm()
+    {
+        $form = $this->getRestoreFormObject();
+        $formView = $this->templateInit('Cabinet/Structure/Part/Site/AccountForms/restoreForm.twig');
+        $formView->start = $form->start();
+        $formHtml = $formView->render();
+        return $formHtml;
+    }
+
+    /**
+     * Генерирует объект формы восстановления пароля
+     *
+     * @return \FormPhp\Forms
+     * @throws \Exception
+     */
+    public function getRestoreFormObject()
+    {
+        $form = new FormPhp\Forms('restoreForm');
+        $form->setAjaxUrl('/?mode=ajax&controller=\\\\Cabinet\\\\Structure\\\\Part\\\\Site&action=restore');
+        $form->add('login', 'text');
+        $form->setValidator('login', 'required');
+        $form->setValidator('login', 'email');
+        return $form;
+    }
+
+    /**
+     * Генерирует html формы регистрации
+     *
+     * @return string
+     */
+    public function getRegistrationForm()
+    {
+        $form = $this->getRegistrationFormObject();
+        $formView = $this->templateInit('Cabinet/Structure/Part/Site/AccountForms/registrationForm.twig');
+        $formView->start = $form->start();
+        $formView->link = $this->link;
+        $formHtml = $formView->render();
+        return $formHtml;
+    }
+
+    /**
+     * Генерирует объект формы регистрации
+     *
+     * @return \FormPhp\Forms
+     * @throws \Exception
+     */
+    public function getRegistrationFormObject()
+    {
+        $form = new FormPhp\Forms('registrationForm');
+        $form->setAjaxUrl('/?mode=ajax&controller=\\\\Cabinet\\\\Structure\\\\Part\\\\Site&action=registration');
+        $form->setClearForm(false);
+        $form->add('lastname', 'text');
+        $form->add('name', 'text');
+        $form->add('phone', 'text');
+        $form->add('addr', 'text');
+        $form->add('email', 'text');
+        $form->add('int', 'text');
+        $form->add('link', 'text');
+        $form->setValidator('lastname', 'required');
+        $form->setValidator('name', 'required');
+        $form->setValidator('phone', 'required');
+        $form->setValidator('phone', 'phone');
+        $form->setValidator('addr', 'required');
+        $form->setValidator('email', 'required');
+        $form->setValidator('email', 'email');
+        $form->setValidator('int', 'required');
+        $form->setValidator('int', 'captcha');
+        return $form;
+    }
+
+    /**
+     * Закрытый метод для генерации отдельного экземпляра класса "Ideal\Core\View"
+     * Используется для получения представления форм с помошью шаблонизатора Twig.
+     *
+     * @param string $tplName Имя шаблона
+     * @return \Ideal\Core\View
+     */
+    protected function templateInit($tplName = '')
+    {
+        if (!stream_resolve_include_path($tplName)) {
+            echo 'Нет файла шаблона ' . $tplName;
+            exit;
+        }
+        $tplRoot = dirname(stream_resolve_include_path($tplName));
+        $tplName = basename($tplName);
+
+        // Определяем корневую папку системы для подключение шаблонов из любой вложенной папки через их путь
+        $config = Config::getInstance();
+        $cmsFolder = DOCUMENT_ROOT . '/' . $config->cmsFolder;
+
+        $folders = array_merge(array($tplRoot, $cmsFolder));
+        $view = new View($folders, $config->cache['templateSite']);
+        $view->loadTemplate($tplName);
+        return $view;
+    }
+}
---
+++ Cabinet/Structure/User/ModelAbstract.php
@@ -0,0 +1,307 @@
+<?php
+/**
+ * Ideal CMS (http://idealcms.ru/)
+ *
+ * @link      http://github.com/ideals/idealcms репозиторий исходного кода
+ * @copyright Copyright (c) 2012-2015 Ideal CMS (http://idealcms.ru/)
+ * @license   http://idealcms.ru/license.html LGPL v3
+ */
+
+namespace Cabinet\Structure\User;
+
+use Ideal\Core\Db;
+use Ideal\Core;
+
+/**
+ * Класс для работы со сторонними пользователями в системе
+ */
+class ModelAbstract extends Core\Model
+{
+
+    /**
+     * Отдаёт информацию о залогиненном пользователе либо false
+     *
+     * @return bool|mixed
+     */
+    public function getLoggedUser()
+    {
+        if (function_exists('session_status')) {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+        } else {
+            if (session_id() == '') {
+                session_start();
+            }
+        }
+        if (isset($_SESSION['login']['is_active']) && $_SESSION['login']['is_active']) {
+            return $this->getUser();
+        }
+        return false;
+    }
+
+    /**
+     * Пытается получить данные пользователя из базы
+     *
+     * @param string $email Значение электронной почты (если известно), false в противном случае
+     * @return mixed false или массив с данными о пользователе
+     */
+    public function getUser($email = '')
+    {
+        $db = Db::getInstance();
+        $fields = array('table' => $this->_table);
+        if (!empty($email)) {
+            $par = array('email' => strtolower($email));
+            $_sql = "SELECT * FROM &table WHERE email= :email LIMIT 1";
+        } else {
+            $par = array('ID' => $_SESSION['login']['ID']);
+            $_sql = "SELECT * FROM &table WHERE ID= :ID LIMIT 1";
+        }
+        $result = $db->select($_sql, $par, $fields);
+        if (count($result) > 0) {
+            return $result[0];
+        }
+        return false;
+    }
+
+    /**
+     * Авторизует пользователя в системе
+     *
+     * @param string $email Адрес электронной почты
+     * @param string $pass Пароль
+     * @return string Ответ на попытку авторизации
+     */
+    public function userAuthorization($email = '', $pass = '')
+    {
+        $response = 'Предоставлены не верные данные';
+        if (!empty($email) && !empty($pass)) {
+            $firstStepCheck = true;
+            if (isset($_SESSION['login']['input']) && $_SESSION['login']['input']) {
+                if ($email != $_SESSION['login']['user']) {
+                    $response = 'Пользователь с указанными данными ещё не зарегистрирован';
+                    $firstStepCheck = false;
+                } elseif (!$_SESSION['login']['is_active']) {
+                    $response = 'Пользователь с указанными данными ещё не активирован';
+                    $firstStepCheck = false;
+                }
+            }
+            if ($firstStepCheck) {
+                $userData = $this->getUser($email);
+                if ($userData) {
+                    if (crypt($pass, $userData['password']) === $userData['password']) {
+                        $_SESSION['login']['user'] = $email;
+                        $_SESSION['login']['ID'] = $userData['ID'];
+                        $_SESSION['login']['input'] = true;
+                        if (function_exists('boolval')) {
+                            $_SESSION['login']['is_active'] = boolval($userData['is_active']);
+                        } else {
+                            if ($userData['is_active']) {
+                                $_SESSION['login']['is_active'] = true;
+                            } else {
+                                $_SESSION['login']['is_active'] = false;
+                            }
+                        }
+                        if (!empty($userData['basket'])) {
+                            $basket = unserialize($userData['basket']);
+                            if ($basket->count > 0) {
+                                $_SESSION['login']['basket'] = json_encode($basket, JSON_FORCE_OBJECT);
+                            }
+                        }
+                        $response = 'Вы успешно вошли';
+                    } else {
+                        $response = 'Ошибка в логине(email) или пароле';
+                    }
+                } else {
+                    $response = 'Пользователя с указанными данными не существует';
+                }
+            }
+        }
+        return $response;
+    }
+
+    /**
+     * Сохраняет изменённые данные пользователя
+     *
+     * @param array $userData Данные с индивидуальной страницы пользователя
+     *
+     * @return string Ответ на попытку сохранения данных о пользователе
+     */
+    public function saveUserData(array $userData)
+    {
+        $response = 'Предоставлены не верные данные';
+        if (!empty($userData)) {
+            $update = array_filter($userData);
+            $db = Db::getInstance();
+            if (isset($update['password'])) {
+                $update['password'] = $db->real_escape_string($update['password']);
+                if (strlen($update['password']) > 0) {
+                    $update['password'] = crypt($update['password']);
+                }
+            }
+            $db->update($this->_table)
+                ->set($update)
+                ->where('ID = :ID', array('ID' => $_SESSION['login']['ID']))
+                ->exec();
+            $response = 'Данные сохранены';
+        }
+        return $response;
+    }
+
+    /**
+     * Восстановление пароля
+     *
+     * @param string $email Электронная почта
+     *
+     * @return array Ответ на попытку восстановления пароля, а так же новый сгенерированны пароль для пользователя
+     */
+    public function restorePassword($email = '')
+    {
+        $response = array(
+            'success' => false,
+            'text' => 'Не известно для кого восстанавливать пароль',
+            'pass' => '',
+        );
+        if (!empty($email)) {
+            $db = Db::getInstance();
+            $par = array('email' => $email);
+            $fields = array('table' => $this->_table);
+            $user = $db->select("SELECT ID FROM &table WHERE email= :email LIMIT 1", $par, $fields);
+            if (count($user) == 0) {
+                $response = array(
+                    'success' => false,
+                    'text' => 'Данный E-mail еще не зарегистрирован',
+                    'pass' => '',
+                    'key' => ''
+                );
+            } else {
+                $pass = $this->randPassword();
+                $db->update($this->_table)->set(array('password' => crypt($pass)))->where('email = :email', array
+                (
+                    'email' => $email
+                ))->exec();
+                $response = array(
+                    'success' => true,
+                    'text' => 'Пароль обновлён',
+                    'pass' => $pass,
+                );
+            }
+        }
+        return $response;
+    }
+
+    /**
+     * Регистрация нового пользователя в системе
+     *
+     * @param array $userData Данные пользователя желающего зарегистрироваться
+     *
+     * @return array Ответ на попытку региистрации в системе, а так же пароль сгенерированны для нового пользователя и ключ авторизации
+     */
+    public function userRegistration(array $userData)
+    {
+        $response = array(
+            'success' => false,
+            'text' => 'Нет данных о новом пользователе',
+            'pass' => '',
+            'key' => ''
+        );
+        if (!empty($userData)) {
+            if (!isset($userData['email']) || !isset($userData['address']) || !isset($userData['phone']) || !isset($userData['fio'])) {
+                $response = array(
+                    'success' => false,
+                    'text' => 'Недостаточно данных для регистрации нового пользователя',
+                    'pass' => '',
+                    'key' => ''
+                );
+            } else {
+                $db = Db::getInstance();
+                $par = array('email' => strtolower($userData['email']));
+                $fields = array('table' => $this->_table);
+                $tmp = $db->select("SELECT ID FROM &table WHERE email= :email LIMIT 1", $par, $fields);
+                if (count($tmp) > 0) {
+                    $response = array(
+                        'success' => false,
+                        'text' => 'Такой Email уже зарегестрирован',
+                            'pass' => '',
+                            'key' => ''
+                        );
+                    } else {
+                        $key = md5(time());
+                    $pass = $this->randPassword();
+                    $db->insert($this->_table, array(
+                        'email' => $userData['email'],
+                        'address' => $userData['address'],
+                        'phone' => $userData['phone'],
+                        'password' => $pass,
+                        'fio' => $userData['fio'],
+                        'is_active' => 0,
+                            'prev_structure' => $this->getPrevStructure(),
+                            'act_key' => $key,
+                        'reg_date' => time()
+                        ));
+
+                        $response = array(
+                            'success' => true,
+                            'text' => 'Пользователь успешно зарегистрирован',
+                        'pass' => $pass,
+                            'key' => $key
+                        );
+                    }
+                }
+            }
+        return $response;
+    }
+
+    /**
+     * Генерация пароля
+     * @param int $min Минимальное количество в пароле
+     * @param int $max Максимальное количество в пароле
+     * @return string
+     */
+    static public function randPassword($min = 8, $max = 12)
+    {
+        $length = rand($min, $max);
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        return substr(str_shuffle($chars), 0, $length);
+    }
+
+    /**
+     * Завершение регистрации
+     *
+     * @return bool Ответ на попытку активации пользователя
+     */
+    public function finishReg()
+    {
+        $db = Db::getInstance();
+        $email = mysqli_real_escape_string($db, $_GET['email']);
+        $key = mysqli_real_escape_string($db, $_GET['key']);
+        $_sql = "SELECT * FROM {$this->_table} WHERE email='{$email}' AND act_key='{$key}' LIMIT 1";
+        $result = $db->select($_sql);
+        if (count($result) == 1) {
+            $_sql = "UPDATE {$this->_table} SET is_active=1, act_key=''";
+            $db->query($_sql);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Выход пользователя
+     */
+    public static function logout()
+    {
+        // TODO Проверяем подключена ли структуры корзины.
+        // Если корзина не пуста, то вызываем метод её сохранения для выходящего пользователя
+        // и очищаем куку корзины
+        // header("Set-Cookie: basket=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT");
+        if (function_exists('session_status')) {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+        } else {
+            if (session_id() == '') {
+                session_start();
+            }
+        }
+        unset($_SESSION['login']);
+    }
+}
