031f15b926fc9765efd8712e5c17ff893000c346
Валентин Ломов <devnull@localhost>
2016-02-15 17:37:57 +0500

    UPD: Получение информации об офферах на странице товара

--- CatalogPlus/Structure/Good/Site/ControllerAbstract.php
+++ CatalogPlus/Structure/Good/Site/ControllerAbstract.php
@@ -1,6 +1,18 @@
 <?php
 namespace CatalogPlus\Structure\Good\Site;
 
+use Ideal\Core\Config;
+
 class ControllerAbstract extends \Ideal\Core\Site\Controller
 {
+    public function indexAction()
+    {
+        parent::indexAction();
+        $config = Config::getInstance();
+        $offers = $config->getStructureByName('CatalogPlus_Offer');
+        if ($offers) {
+            $pageData = $this->model->getPageData();
+            $this->view->offers = $this->model->getGoodsInfo(array($pageData['ID']));
+        }
+    }
 }
