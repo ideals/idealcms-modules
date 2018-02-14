9734025e168f8f370f0ecdd10470e51517015195
Валентин Ломов <devnull@localhost>
2018-02-14 12:43:00 +0500

    FIX: правильное получение списка категорий для фильтрации товаров + метод для уточнения выборки товаров по выбранной категории

--- Shop/Structure/Category/Getters/CategoryList.php
+++ Shop/Structure/Category/Getters/CategoryList.php
@@ -21,12 +21,13 @@ class CategoryList
         $db = Db::getInstance();
         $config = Config::getInstance();
         $_table = $config->db['prefix'] . 'shop_structure_category';
-        $_sql = "SELECT ID, name FROM {$_table}";
+        $_sql = "SELECT ID, name, lvl FROM {$_table} ORDER BY cid";
         $arr = $db->select($_sql);
 
         $list = array();
         foreach ($arr as $item) {
-            $list[$item['ID']] = $item['name'];
+            $dash = str_repeat('-', intval($item['lvl']) - 1);
+            $list[$item['ID']] = $dash . $item['name'];
         }
 
         return $list;
@@ -60,6 +61,14 @@ class CategoryList
     {
         $_sql = "DELETE FROM i_shop_category_good WHERE good_id='{{ objectId }}';"
               . "INSERT INTO i_shop_category_good SET good_id='{{ objectId }}', category_id='{$newValue}';";
+        return $_sql;
+    }
+
+    public function getWhereFilter($currentCategory)
+    {
+        $db = Db::getInstance();
+        $_sql = "ID IN (SELECT good_id FROM i_shop_category_good WHERE category_id="
+            . $db->escape_string($currentCategory) . ')';
         return $_sql;
     }
 
