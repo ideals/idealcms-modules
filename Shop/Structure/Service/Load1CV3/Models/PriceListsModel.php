66eab15cbebdf64330e8f031b5f50f771ccf3cbb
Paul Nikitin <paulnikitin@gmail.com>
2024-08-03 20:45:41 +0300

    FIX: обработка прайс-листов при загрузке из 1С

--- Shop/Structure/Service/Load1CV3/Models/PriceListsModel.php
+++ Shop/Structure/Service/Load1CV3/Models/PriceListsModel.php
@@ -3,14 +3,22 @@
 namespace Shop\Structure\Service\Load1CV3\Models;
 
 use Shop\Structure\Service\Load1CV3\ModelAbstract;
+use Shop\Structure\Service\Load1CV3\Xml\PriceLists\XmlPriceLists;
+use Shop\Structure\Service\Load1CV3\Xml\Xml;
 
 class PriceListsModel extends ModelAbstract
 {
+    private XmlPriceLists $xmlPriceLists;
 
     public function init(): void
     {
         $this->setInfoText('Обработка типов цен каталога (priceLists)');
         $this->setSort(10);
+
+        // инициализируем модель типов цен в XML
+        $xml = new Xml($this->filename);
+        $this->xmlPriceLists = new XmlPriceLists($xml);
+        $this->isOnlyUpdate = $this->xmlPriceLists->updateInfo();
     }
 
     public function startProcessing($packageNum): array
